2f21d0b88a5760f48119bee547d2e0c7c12b33ee
ProjectX2019 <risman.slava@gmail.com>
2024-07-13 14:20:07 +0300

    This is Targil 1

---
+++ Lesson1/Targil1.py
@@ -0,0 +1,5 @@
+age = input("Your age is :")
+name = input("Enter Your name: ")
+mail = input("Enter your Email: ")
+print("Full name: " + name + "\nyour age is: " + str(age) + "\nYour mail is: " + mail)
+
